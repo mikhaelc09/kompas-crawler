77825bd95e1fde1c18648a0e1ea88f138324361e
mikhaelc09 <mikhael.chris09@gmail.com>
2023-02-14 22:06:15 +0700

    scrap each article from page

--- scraper.py
+++ scraper.py
@@ -4,7 +4,8 @@ from bs4 import BeautifulSoup
 BASE_URL = "https://sorotpolitik.kompas.com/"
 
 def get_articles(page):
-    html = requests.get(BASE_URL).text
+    url = BASE_URL + ("/{page}" if page > 1 else "")
+    html = requests.get(url).text
     soup = BeautifulSoup(html, 'html.parser')
     content = soup.find_all("a", {
         "class" : "article__link"
@@ -18,15 +19,12 @@ def scrap_article(url):
         "class" : "read__content"
     })[0]
     para = content.find_all("p")
-    for i, p in enumerate(para):
-        if(i > 0 and len(p.text) > 0):
-            print(i," : ",p.text)
 
-    return para
+    return list(map(lambda x: x.text,para))
 
-# url = "https://sorotpolitik.kompas.com/memilih-pemimpin-negeri/read/2019/04/10/08470001/terbukti-jokowi-menaruh-harapan-besar-pada-pns-muda?utm_source=Kompas&utm_medium=Media&utm_campaign=Hasilketrampilan"
-# scrap_article(url)
-
-articles = get_articles(0)
-for i,a in enumerate(articles):
-    print(i,' : ',a.attrs['href'])
+articles = get_articles(1)
+for index,article in enumerate(articles):
+    print(index,' : ',article.text)
+    content = scrap_article(article.attrs["href"])
+    print(content)
+    
