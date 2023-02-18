45a12581b780595d51fd80d7b5ea363942e33e70
mikhaelc09 <mikhael.chris09@gmail.com>
2023-02-19 02:28:10 +0700

    create corpus reader

--- iocorpus.py
+++ iocorpus.py
@@ -1,5 +1,6 @@
 import datetime
-
+import re
+from Article import Article
 
 def save_to_file(documents):
     today_date = datetime.datetime.now().strftime("%d%m%y%H%M%S")
@@ -13,6 +14,26 @@ def save_to_file(documents):
         f.write('</corpus>')
 
 def read_corpus(filename):
+    articles = []
     with open(filename, 'r') as f:
         for i, x in enumerate(f):
-            print(i,x)
+            x = x.replace('\t', '')
+            tag = x[1:x.find('>')]
+            tokenized_tag = tag.split(' ')
+        
+            if(tokenized_tag[0][0] == '/') :
+                pass
+            elif(tokenized_tag[0] == 'corpus'):
+                pass
+            elif (tokenized_tag[0] == 'doc'):
+                articles.append(Article(tokenized_tag[2][4:], []))
+            elif (tokenized_tag[0] == 's'):
+                sentence = x[x.find('>')+1:x.find('<',2)]
+                articles[-1].sentences.append(sentence)
+
+    return articles
+            
+            
+arts = read_corpus('kompas_180223195900.txt')
+for i in arts:
+    print(i)
