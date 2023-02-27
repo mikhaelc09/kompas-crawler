c9eb65a1908c31a862dc577cf61042bb642fa140
Cherilyn Eugenia <cherilyn.eugenia@gmail.com>
2023-02-27 19:47:33 +0700

    fix probability

--- languageModel.py
+++ languageModel.py
@@ -44,16 +44,20 @@ bigram_freq = [] #array of Bigram
 #get word frequency
 def get_words_freq(words):
     print('Calculating word frequencies...')
+    word_freq = []
     for i in words:
         found = False
         
-        for j in word_freq:
-            if j.word==i:
-                j.addFreq()
-                found = True
-                break 
-            
-        if found==False:
+        if len(word_freq)>0:
+            for j in word_freq:
+                if j.word==i:
+                    j.addFreq()
+                    found = True
+                    break 
+                
+            if found==False:
+                word_freq.append(Word(i))
+        else:
             word_freq.append(Word(i))
     
     # for i in word_freq:
@@ -85,16 +89,20 @@ def generate_ngram(words, n):
 #get frequency of n-gram
 def get_ngrams_freq(bigrams):
     print('Calculating n-gram frequencies...')
+    bigram_freq = []
     for i in bigrams:
         found = False
 
-        for j in bigram_freq:
-            if j.word1==i[0] and j.word2==i[1]:
-                j.addFreq()
-                found = True
-                break 
-            
-        if found==False:
+        if(len(bigram_freq)>0):
+            for j in bigram_freq:
+                if j.word1==i[0] and j.word2==i[1]:
+                    j.addFreq()
+                    found = True
+                    break 
+                
+            if found==False:
+                bigram_freq.append(Bigram(i[0], i[1]))
+        else:
             bigram_freq.append(Bigram(i[0], i[1]))
         
     # for i in bigram_freq:
@@ -106,7 +114,8 @@ def get_bigram_freq(word1, word2, bigram_freq):
     freq = 1
     for i in bigram_freq:
         if i.word1==word1 and i.word2==word2:
-            freq = i.freq
+            freq += i.freq
+            break
     
     return freq
         
@@ -120,20 +129,21 @@ def calculate_all_probability(word_freq, bigram_freq):
                 
                 prob = (f_bigram / i.freq)
                 # print(prob)
-                i.addProbability(Probability(j,prob))
+                i.addProbability(Probability(j.word,prob))
     
     return word_freq
                 
 #get probability of 2 words
 def get_probability(word1, word2, word_freq):
-    if word1 in word_freq and word2 in word_freq:
+    prob = 0.001
+    if any(x.word==word1 for x in word_freq) and any(x.word==word2 for x in word_freq):
         for i in word_freq:
             if i.word==word1:
                 for j in i.probabilities:
                     if j.word==word2:
-                        return j.probability
-    else:
-        return 1
+                        prob = j.probability
+    
+    return prob
     
 
 #get next word (word with highest probability)
@@ -160,11 +170,12 @@ def get_next_word(sentence, word_freq):
     
     for i in word_freq:
         prob1 = get_probability(tokens[len(tokens)-1], i.word, word_freq)
-        # prob2 = get_probability(i, "</s>")
+        # prob2 = get_probability(i.word, "</s>", word_freq)
         # if tempProb < prob1*prob2:
         #     tempProb = prob1*prob2
         #     nextWord = i.word
-        print(prob1)
+        # print(prob1, prob2)
+        # print(prob1)
         if tempProb < prob1:
             nextWord = i.word
             tempProb = prob1
@@ -177,6 +188,8 @@ def get_next_word(sentence, word_freq):
 
 
 #main program
+words = []
+bigrams = []
 for i in arts:
     for j in i.sentences:
         words = word_tokenize(j)
@@ -187,12 +200,12 @@ for i in arts:
             all_words.append(k)
             
         bigram = generate_ngram(words, 2)
-        bigrams += bigram
+        bigrams.extend(bigram)
 
 word_freq = get_words_freq(all_words)
 bigram_freq = get_ngrams_freq(bigrams)
-word_freq = calculate_all_probability(word_freq, bigram_freq)
+word_freq_prob = calculate_all_probability(word_freq, bigram_freq)
 
 #ask for input, return next word of that sentence
 sentence = input("Enter a sentence: ")
-get_next_word(sentence, word_freq)
+get_next_word(sentence, word_freq_prob)
